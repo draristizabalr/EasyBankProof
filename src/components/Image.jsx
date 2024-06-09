f226fc01856ff58e9f4077b83e45b8891905d15a
david.aristizabal <david.aristizabal@kiplan.io>
2024-06-09 03:45:43 -0500

    Fixed size from Image

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -7,7 +7,7 @@ export function Image ({ className }) {
       <img
         src='/images/bg-intro-desktop.svg'
         alt='background desktop image'
-        className='hidden sm:block w-full h-auto scale-[1.75]'
+        className='hidden sm:block w-full h-auto scale-[1.25] transform -translate-y-24 translate-x-16'
       />
       <img
         src='/images/bg-intro-mobile.svg'
@@ -17,7 +17,7 @@ export function Image ({ className }) {
       <img
         src='/images/image-mockups.png'
         alt='mobiles image'
-        className='absolute w-[60%] sm:w-full h-auto transform -translate-x-1/2 sm:-translate-x-0 left-1/2 sm:left-0 top-12 sm:top-0 scale-125 overflow-visible'
+        className='absolute w-[60%] sm:w-[70%] h-auto transform -translate-x-1/2 sm:translate-x-56 left-1/2 sm:left-0 top-12 sm:-top-4 scale-125 overflow-visible'
       />
     </div>
   )
